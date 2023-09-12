7c3e7454ba064b41a28f28fc005b0e2cfa95f3c3
akbarnuri <makerctvvip@gmail.com>
2023-09-12 09:58:30 +0700

    app say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/akbarnuri/appsay-hello
+
+go 1.21.1
+
+require github.com/akbarnuri/gosay_hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	gosay_hello "github.com/akbarnuri/gosay_hello/v2"
+)
+
+func main() {
+	fmt.Println(gosay_hello.SayHello(" akbar"))
+}
